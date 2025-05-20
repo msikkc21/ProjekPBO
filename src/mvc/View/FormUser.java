d65ef2785fabfd2f919795da8f5330af67ee8789
msikkc21 <izzaaida21@gmail.com>
2025-05-20 21:28:38 +0700

    first commit

---
+++ src/mvc/View/FormUser.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mvc.View;
+
+import mvc.Koneksi.Koneksi;
+
+/**
+ *
+ * @author izzaa
+ */
+public class FormUser {
+    public static void main(String[] args) {
+        Koneksi.getConnection();
+    }
+}
