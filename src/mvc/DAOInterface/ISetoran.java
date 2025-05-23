8c31ebf8767bf8b411eabe54c3948b42817bf2fc
John Doe <johndoe@example.com>
2025-05-23 14:02:26 +0700

    tambah setoran

---
+++ src/mvc/DAOInterface/ISetoran.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mvc.DAOInterface;
+
+import java.util.List;
+import mvc.Model.Setoran;
+
+/**
+ *
+ * @author Acer
+ */
+public interface ISetoran {
+    public void insert (Setoran a);
+    public void update(Setoran a);
+    public void delete(int id);
+    public List<Setoran>getAll();
+    public List<Setoran> getCariSantri(String nama);
+    
+}
--- src/mvc/Koneksi/Koneksi.java
+++ src/mvc/Koneksi/Koneksi.java
@@ -30,4 +30,6 @@ public class Koneksi {
         }
         return con;
     }
+
+   
 }
